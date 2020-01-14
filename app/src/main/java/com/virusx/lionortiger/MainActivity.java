473061c7ff65c00c5e7d0b233335ffe2f1c57054
thisisfaisalhere <faisalismoodie@gmail.com>
2020-01-14 14:36:53 +0530

    adding feature complete and rebased into master feature added: android play

--- app/src/main/java/com/virusx/lionortiger/MainActivity.java
+++ app/src/main/java/com/virusx/lionortiger/MainActivity.java
@@ -2,7 +2,6 @@ package com.virusx.lionortiger;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.gridlayout.widget.GridLayout;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
